0e670936dc6ef14cf2fbc5cad814e8eaa00c360d
MoaresOliveira <moaresoliveira@gmail.com>
2022-07-11 20:44:44 -0300

    First Commit

---
+++ src/main/java/io/github/greenizer/GreenizerApplication.java
@@ -0,0 +1,13 @@
+package io.github.greenizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/greenizer/GreenizerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.greenizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
